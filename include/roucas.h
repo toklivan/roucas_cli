bf7cc9c944e0bbad20c2885d7604ced342aee5b8
Livan Tok <ltok@c2r7s4.marseille.42.school>
2026-01-13 12:00:37 +0100

    feat: define product data model (t_product)

---
+++ include/roucas.h
@@ -0,0 +1,16 @@
+#ifndef ROUCAS_H
+# define ROUCAS_H
+
+typedef struct s_product
+{
+    char                *id;
+    char                *name;
+    char                *category;
+    double              buy_price;
+    double              sell_price;
+    int                 stock;
+    int                 low_threshold;
+    struct s_product    *next;
+}   t_product;
+
+#endif
