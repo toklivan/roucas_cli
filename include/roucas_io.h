216e8b9315e444094fdd0294e717a94352334458
Livan Tok <ltok@c1r3s3.marseille.42.school>
2026-01-15 13:53:53 +0100

    feat: load_products opens CSV file (step 1)

--- include/roucas_io.h
+++ include/roucas_io.h
@@ -3,9 +3,9 @@
 
 # include "roucas.h"
 
-int         load_products(const char *path, t_product **products);
-int         save_products(const char *path, t_product *products);
-void        free_products(t_product *products);
-t_product   *find_product_by_id(t_product *products, const char *id);
+int			load_products(const char *path, t_product **products);
+int			save_products(const char *path, t_product *products);
+void		free_products(t_product *products);
+t_product	*find_product_by_id(const char *id, t_product *products);
 
 #endif
--- src/roucas_io.c
+++ src/roucas_io.c
@@ -3,10 +3,19 @@
 
 int	load_products(const char *path, t_product **products)
 {
-	(void)path;
-	(void)products;
-	fprintf(stderr, "load_products: not implemented yet\n");
-	return (1);
+	FILE	*fp;
+
+	if (!path || !products)
+		return (1);
+	*products = NULL;
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		perror("roucas_cli: fopen");
+		return (1);
+	}
+	fclose(fp);
+	return (0);
 }
 
 int	save_products(const char *path, t_product *products)
